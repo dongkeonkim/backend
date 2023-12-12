fce8a38a82c61553f7c7e14b08834f5fa2b5c2ef
dkkim <dongkeonkim0810@gmail.com>
2023-12-12 22:46:38 +0900

    fix: 잔디 테스트

--- src/main/java/com/palette/backend/board/controller/BoardController.java
+++ src/main/java/com/palette/backend/board/controller/BoardController.java
@@ -1,8 +1,14 @@
 package com.palette.backend.board.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/board")
 public class BoardController {
+
+    @GetMapping("/list")
+    public String test() {
+        return "test";
+    }
 
 }
