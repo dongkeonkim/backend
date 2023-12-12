8e9df4f69a1e4712eebb2aaf3153712afa8b4940
dongkeonkim <dongkeonkim0810@gmail.com>
2023-12-12 22:52:06 +0900

    Revert "fix: 잔디 테스트"
    This reverts commit fce8a38a82c61553f7c7e14b08834f5fa2b5c2ef.

--- src/main/java/com/palette/backend/board/controller/BoardController.java
+++ src/main/java/com/palette/backend/board/controller/BoardController.java
@@ -1,14 +1,8 @@
 package com.palette.backend.board.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/board")
 public class BoardController {
-
-    @GetMapping("/list")
-    public String test() {
-        return "test";
-    }
 
 }
